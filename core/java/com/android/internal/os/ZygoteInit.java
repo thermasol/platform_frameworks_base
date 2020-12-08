329521b95f06e903289265a50da2ecf94eabe7b5
Blake W. Ford <blake@thermasol.com>
2020-12-08 14:04:02 -0600

    Reapply patches

--- core/java/com/android/internal/os/ZygoteInit.java
+++ core/java/com/android/internal/os/ZygoteInit.java
@@ -69,6 +69,8 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.security.Provider;
 import java.security.Security;
+import java.util.ArrayList;
+import java.util.concurrent.CountDownLatch;
 
 /**
  * Startup class for the zygote process.
@@ -126,6 +128,9 @@ public class ZygoteInit {
 
     private static boolean sPreloadComplete;
 
+    private static CountDownLatch pclassCountDown;
+    private static CountDownLatch ptextCountDown;
+
     /**
      * Cached classloader to use for the system server. Will only be populated in the system
      * server process.
@@ -143,6 +148,7 @@ public class ZygoteInit {
         bootTimingsTraceLog.traceBegin("CacheNonBootClasspathClassLoaders");
         cacheNonBootClasspathClassLoaders();
         bootTimingsTraceLog.traceEnd(); // CacheNonBootClasspathClassLoaders
+        preloadTextResources();
         bootTimingsTraceLog.traceBegin("PreloadResources");
         preloadResources();
         bootTimingsTraceLog.traceEnd(); // PreloadResources
@@ -153,7 +159,6 @@ public class ZygoteInit {
         maybePreloadGraphicsDriver();
         Trace.traceEnd(Trace.TRACE_TAG_DALVIK);
         preloadSharedLibraries();
-        preloadTextResources();
         // Ask the WebViewFactory to do any initialization that must run in the zygote process,
         // for memory sharing purposes.
         WebViewFactory.prepareWebViewInZygote();
@@ -208,8 +213,18 @@ public class ZygoteInit {
     }
 
     private static void preloadTextResources() {
-        Hyphenator.init();
-        TextView.preloadFontCache();
+        class PreloadTextResourceThread  extends Thread {
+        @Override
+            public void run() {
+                Hyphenator.init();
+                TextView.preloadFontCache();
+                ptextCountDown.countDown();
+            }
+        }
+
+        ptextCountDown = new CountDownLatch(1);
+        PreloadTextResourceThread thread = new PreloadTextResourceThread();
+        thread.start();
     }
 
     /**
@@ -248,6 +263,42 @@ public class ZygoteInit {
      * Kbytes (in one case, 500+K).
      */
     private static void preloadClasses() {
+
+        class PreloadClassThread extends Thread {
+            ArrayList<String> preloadList;
+            public void setList(ArrayList<String> list) {
+                this.preloadList = list;
+            }
+
+            @Override
+            public void run() {
+                try {
+                    for (String line: preloadList) {
+                        try {
+                            Class.forName(line, true, null);
+                        } catch (ClassNotFoundException e) {
+                            Log.w(TAG, "Class not found for preloading: " + line);
+                        } catch (UnsatisfiedLinkError e) {
+                            Log.w(TAG, "Problem preloading " + line + ": " + e);
+                        } catch (Throwable t) {
+                            Log.e(TAG, "Error preloading " + line + ".", t);
+                            if (t instanceof Error) {
+                                throw (Error) t;
+                            }
+                            if (t instanceof RuntimeException) {
+                                throw (RuntimeException) t;
+                            }
+                            throw new RuntimeException(t);
+                        }
+                    }
+                } catch (Exception err) {
+                    throw err;
+                } finally {
+                    pclassCountDown.countDown();
+                }
+            }
+        }
+
         final VMRuntime runtime = VMRuntime.getRuntime();
 
         InputStream is;
@@ -285,6 +336,11 @@ public class ZygoteInit {
         float defaultUtilization = runtime.getTargetHeapUtilization();
         runtime.setTargetHeapUtilization(0.8f);
 
+        ArrayList<String> list_all = new ArrayList<String>();
+        ArrayList<String> list = new ArrayList<String>();
+        ArrayList<ArrayList<String>> task_list = new ArrayList<ArrayList<String>>();
+        int list_len = 0;
+
         try {
             BufferedReader br =
                     new BufferedReader(new InputStreamReader(is), Zygote.SOCKET_BUFFER_SIZE);
@@ -293,38 +349,28 @@ public class ZygoteInit {
             String line;
             while ((line = br.readLine()) != null) {
                 // Skip comments and blank lines.
-                line = line.trim();
-                if (line.startsWith("#") || line.equals("")) {
-                    continue;
+                //Split to multiple thread. Each thread handle 1600 classes.
+                list.add(line);
+                list_len ++;
+                if (list_len >= 1600) {
+                    list = new ArrayList<String>();
+                    list_len = 0;
+                    task_list.add(list);
                 }
 
                 Trace.traceBegin(Trace.TRACE_TAG_DALVIK, line);
-                try {
-                    if (false) {
-                        Log.v(TAG, "Preloading " + line + "...");
-                    }
-                    // Load and explicitly initialize the given class. Use
-                    // Class.forName(String, boolean, ClassLoader) to avoid repeated stack lookups
-                    // (to derive the caller's class-loader). Use true to force initialization, and
-                    // null for the boot classpath class-loader (could as well cache the
-                    // class-loader of this class in a variable).
-                    Class.forName(line, true, null);
-                    count++;
-                } catch (ClassNotFoundException e) {
-                    Log.w(TAG, "Class not found for preloading: " + line);
-                } catch (UnsatisfiedLinkError e) {
-                    Log.w(TAG, "Problem preloading " + line + ": " + e);
-                } catch (Throwable t) {
-                    Log.e(TAG, "Error preloading " + line + ".", t);
-                    if (t instanceof Error) {
-                        throw (Error) t;
-                    }
-                    if (t instanceof RuntimeException) {
-                        throw (RuntimeException) t;
-                    }
-                    throw new RuntimeException(t);
-                }
                 Trace.traceEnd(Trace.TRACE_TAG_DALVIK);
+            }
+
+            if (list_len != 0) {
+                list = new ArrayList<String>();
+                task_list.add(list);
+            }
+            pclassCountDown = new CountDownLatch(task_list.size());
+            for(ArrayList<String> onelist: task_list) {
+                PreloadClassThread thread = new PreloadClassThread();
+                thread.setList(onelist);
+                thread.start();
             }
 
             Log.i(TAG, "...preloaded " + count + " classes in "
@@ -377,6 +423,17 @@ public class ZygoteInit {
                     hidlBase,
                     hidlManager,
                 });
+    }
+
+    private static void waitForPreloadAsync() {
+        if (pclassCountDown != null) {
+            try {
+                pclassCountDown.await();
+                ptextCountDown.await();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     /**
@@ -776,6 +833,9 @@ public class ZygoteInit {
                 parsedArgs.mRuntimeFlags |= Zygote.PROFILE_SYSTEM_SERVER;
             }
 
+            /* Make sure all preload process end before start SystemServer */
+            waitForPreloadAsync();
+
             /* Request to fork the system server process */
             pid = Zygote.forkSystemServer(
                     parsedArgs.mUid, parsedArgs.mGid,
@@ -791,7 +851,10 @@ public class ZygoteInit {
         /* For child process */
         if (pid == 0) {
             if (hasSecondZygote(abiList)) {
-                waitForSecondaryZygote(socketName);
+                if (!SystemProperties.getBoolean("vendor.all.car", false))
+                    waitForSecondaryZygote(socketName);
+                else
+                    Slog.d(TAG, "Android Automotive images, no need to wait for secondary zygote");
             }
 
             zygoteServer.closeServerSocket();
@@ -821,7 +884,6 @@ public class ZygoteInit {
 
         // Mark zygote start. This ensures that thread creation will throw
         // an error.
-        ZygoteHooks.startZygoteNoThreadCreation();
 
         // Zygote goes into its own process group.
         try {
@@ -895,8 +957,6 @@ public class ZygoteInit {
 
             Zygote.initNativeState(isPrimaryZygote);
 
-            ZygoteHooks.stopZygoteNoThreadCreation();
-
             zygoteServer = new ZygoteServer(isPrimaryZygote);
 
             if (startSystemServer) {
@@ -911,6 +971,11 @@ public class ZygoteInit {
             }
 
             Log.i(TAG, "Accepting command socket connections");
+
+            if ("1".equals(SystemProperties.get("vendor.all.setup_main.ready"))) {
+                SystemProperties.set("vendor.all.zygote_auto.ready", "1");
+                Log.d(TAG, "Android Automotive second zygote ready now.");
+            }
 
             // The select loop returns early in the child process after a fork and
             // loops forever in the zygote.
--- services/usb/java/com/android/server/usb/UsbDeviceManager.java
+++ services/usb/java/com/android/server/usb/UsbDeviceManager.java
@@ -167,7 +167,7 @@ public class UsbDeviceManager implements ActivityTaskManagerInternal.ScreenObser
     // Delay for debouncing USB disconnects.
     // We often get rapid connect/disconnect events when enabling USB functions,
     // which need debouncing.
-    private static final int UPDATE_DELAY = 1000;
+    private static final int UPDATE_DELAY = 1500;
 
     // Timeout for entering USB request mode.
     // Request is cancelled if host does not configure device within 10 seconds.
@@ -499,6 +499,7 @@ public class UsbDeviceManager implements ActivityTaskManagerInternal.ScreenObser
 
             mCurrentUser = ActivityManager.getCurrentUser();
             mScreenLocked = true;
+            mHideUsbNotification = true;
 
             mSettings = getPinnedSharedPrefs(mContext);
             if (mSettings == null) {
@@ -800,6 +801,7 @@ public class UsbDeviceManager implements ActivityTaskManagerInternal.ScreenObser
                     mConnected = (msg.arg1 == 1);
                     mConfigured = (msg.arg2 == 1);
 
+                    mHideUsbNotification = mHideUsbNotification && (!mConnected);
                     updateUsbNotification(false);
                     updateAdbNotification(false);
                     if (mBootCompleted) {
@@ -866,7 +868,6 @@ public class UsbDeviceManager implements ActivityTaskManagerInternal.ScreenObser
                         Slog.i(TAG, "HOST_STATE connected:" + connected);
                     }
 
-                    mHideUsbNotification = false;
                     while (devices.hasNext()) {
                         Map.Entry pair = (Map.Entry) devices.next();
                         if (DEBUG) {
@@ -882,7 +883,7 @@ public class UsbDeviceManager implements ActivityTaskManagerInternal.ScreenObser
                                 UsbInterface intrface = config.getInterface(interfaceCount);
                                 interfaceCount--;
                                 if (sBlackListedInterfaces.contains(intrface.getInterfaceClass())) {
-                                    mHideUsbNotification = true;
+                                    mHideUsbNotification = mHideUsbNotification && true;
                                     break;
                                 }
                             }
@@ -1448,13 +1449,13 @@ public class UsbDeviceManager implements ActivityTaskManagerInternal.ScreenObser
 
         private boolean waitForState(String state) {
             // wait for the transition to complete.
-            // give up after 1 second.
+            // give up after 4 second.
             String value = null;
             for (int i = 0; i < 20; i++) {
                 // State transition is done when sys.usb.state is set to the new configuration
                 value = getSystemProperty(USB_STATE_PROPERTY, "");
                 if (state.equals(value)) return true;
-                SystemClock.sleep(50);
+                SystemClock.sleep(200);
             }
             Slog.e(TAG, "waitForState(" + state + ") FAILED: got " + value);
             return false;
@@ -1822,8 +1823,13 @@ public class UsbDeviceManager implements ActivityTaskManagerInternal.ScreenObser
                     /**
                      * Dont force to default when the configuration is already set to default.
                      */
-                    if (msg.arg1 != 1) {
-                        setEnabledFunctions(UsbManager.FUNCTION_NONE, !isAdbEnabled());
+
+                    if (mCurrentFunctionsApplied) {
+                        updateState("CONFIGURED");
+                    } else if (msg.arg1 != 1) {
+                        if (msg.arg1 != 1) {
+                            setEnabledFunctions(UsbManager.FUNCTION_NONE, !isAdbEnabled());
+                        }
                     }
                     break;
                 case MSG_GADGET_HAL_REGISTERED:
